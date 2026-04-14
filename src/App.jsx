92388925c927cd374ac8279036b46e5c172d03d5
Marin Stolnik <stolnikmarin@gmail.com>
2026-04-14 22:32:42 +0200

    dodao sam funkcionalnosti

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,30 @@ import pattern2 from "./assets/pattern2.png";
 import pattern3 from "./assets/pattern3.png";
 import logo from "./assets/vite.svg";
 function App() {
+  var [text, setText] = useState("");
+  var slova = text.length;
+  var rijeci = text.trim() == "" ? 0 : text.trim().split(" ").length;
+  var recenice = text.trim() == "" ? 0 : text.trim().split(".").length;
+  const density = () => {
+    if (text.trim() === "") return [];
+
+    const malaslova = text.toLowerCase();
+    const bezrazmaka = malaslova.replaceAll(" ", "").split("");
+
+    let brojac = {};
+    let ukupnoslova = bezrazmaka.length;
+
+    bezrazmaka.forEach((slovo) => {
+      brojac[slovo] = (brojac[slovo] || 0) + 1;
+    });
+
+    const vrijednosti = Object.entries(brojac).map(([slovo, count]) => {
+      const postotak = ((count / ukupnoslova) * 100).toFixed(2);
+      return [slovo, { count, postotak: Number(postotak) }];
+    });
+
+    return vrijednosti.sort((a, b) => b[1].count - a[1].count).slice(0, 5);
+  };
   return (
     <div class="stranica">
       <div class="topbar">
@@ -13,25 +37,32 @@ function App() {
       </div>
       <h1 class="heading">Analyze your text</h1>
       <h1 class="heading">in real-time.</h1>
-      <textarea name="text" id="text" rows={15} class="area"></textarea>
+      <textarea
+        name="text"
+        id="text"
+        rows={15}
+        class="area"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
       <div class="redak">
         <div class="characters">
           <div class="lijevo">
-            <p class="counter">278</p>
+            <p class="counter">{slova}</p>
             <p class="opis">Total Characters</p>
           </div>
           <img class="slika" src={pattern1} alt="slika" />
         </div>
         <div class="word">
           <div class="lijevo">
-            <p class="counter">278</p>
+            <p class="counter">{rijeci}</p>
             <p class="opis">Word Count</p>
           </div>
           <img class="slika" src={pattern2} alt="slika" />
         </div>
         <div class="sentence">
           <div class="lijevo">
-            <p class="counter">278</p>
+            <p class="counter">{recenice}</p>
             <p class="opis">Sentence Count</p>
           </div>
           <img class="slika" src={pattern3} alt="slika" />
@@ -40,13 +71,22 @@ function App() {
       <div class="podnaslov">
         <h1 class="density">Letter density</h1>
       </div>
-      <div class="densityredak">
-        <p>E</p>
-        <div class="progressiza">
-          <div class="progressunutra"></div>
+      {density().map(([slovo, vrijednost], index) => (
+        <div className="densityredak" key={index}>
+          <p>{slovo}</p>
+
+          <div className="progressiza">
+            <div
+              className="progressunutra"
+              style={{ width: `${vrijednost.postotak}%` }}
+            ></div>
+          </div>
+
+          <p>
+            {vrijednost.count} ({vrijednost.postotak}%)
+          </p>
         </div>
-        <p>40(16,06%)</p>
-      </div>
+      ))}
     </div>
   );
 }
